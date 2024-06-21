a7b1bf45ac2e03103a6a7dbd2f5fd44d4643b4f8
christianwedel <christianwedel000@gmail.com>
2024-06-21 22:18:56 +0200

    v0.1

--- basicWpPlugin.php
+++ basicWpPlugin.php
@@ -11,13 +11,14 @@ require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
 use BasicWpPlugin\DatabaseHandler;
 use BasicWpPlugin\Init;
 
-// Function to handle plugin activation
+/* Function to handle plugin activation -> activate if needed
 function basic_wp_plugin_activate() {
     $db_handler = new DatabaseHandler();
     $db_handler->check_and_create_table();
     add_option('basic_wp_plugin_db_created', true); // Set option for admin notice
 }
 register_activation_hook(__FILE__, 'basic_wp_plugin_activate');
+*/
 
 // Initialize the plugin
 function initialize_basic_wp_plugin() {
